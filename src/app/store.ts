205b3dfa983577d344d1b7b9890757b440561c4c
natron2244 <natron2244@gmail.com>
2021-03-05 20:42:49 -0400

    Adding cube slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
+
 import counterReducer from '../features/counter/counterSlice';
+import cubeReducer from '../features/cube/cubeSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    cube: cubeReducer,
   },
 });
 
---
+++ src/features/cube/cubeSlice.ts
@@ -0,0 +1,75 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+import { AppThunk, RootState } from '../../app/store';
+
+interface Card {
+  name: string,
+}
+
+interface Archetype {
+  name: string,
+}
+
+interface Rating {
+  name: string,
+  value: number,
+}
+
+interface CubeState {
+  cards: Card[];
+  archetypes: Archetype[];
+  ratings: Rating[];
+}
+
+const defaultRatings: Rating[] = [
+  {
+    name: '1',
+    value: 1,
+  },
+  {
+    name: '2',
+    value: 2,
+  },
+  {
+    name: '3',
+    value: 3,
+  },
+  {
+    name: '4',
+    value: 4,
+  },
+  {
+    name: '5',
+    value: 5,
+  },
+];
+
+const initialState: CubeState = {
+  cards: [],
+  archetypes: [],
+  ratings: defaultRatings,
+};
+
+export const cubeSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    addCard: (state, action: PayloadAction<Card>) => {
+      state.cards = [...state.cards, action.payload];
+    },
+    addArchetypes: (state, action: PayloadAction<Archetype>) => {
+      state.archetypes = [...state.archetypes, action.payload];
+    },
+    addRating: (state, action: PayloadAction<Rating>) => {
+      state.ratings = [...state.ratings, action.payload];
+    },
+  },
+});
+
+export const { addCard, addArchetypes, addRating } = cubeSlice.actions;
+
+export const selectCards = (state: RootState) => state.cube.cards;
+export const selectArchetypes = (state: RootState) => state.cube.archetypes;
+export const selectRatings = (state: RootState) => state.cube.ratings;
+
+export default cubeSlice.reducer;
